d1e19cc9bf849505639e7eeda3e442373795bb34
John Eberhard <102746950+jeber-ibm@users.noreply.github.com>
2023-08-01 10:47:58 -0500

    Return error count in System.exit() (#24)

--- src/test/RegressionTest.java
+++ src/test/RegressionTest.java
@@ -35,6 +35,7 @@ Main for running standalone application tests.
 **/
   public static void main(String args[])
   {
+	int failureCount = 0; 
     TestDriverStatic.brief_ = true;  // Skip long-running variations.
     try
     {
@@ -80,6 +81,7 @@ Main for running standalone application tests.
        me.init();
        me.start();
        me.stop();
+       failureCount = me.totalFail_; 
        me.destroy();
        me.cleanup(true);
     }
@@ -90,7 +92,7 @@ Main for running standalone application tests.
     }
 
     // Needed to make the virtual machine quit.
-       System.exit(0);
+       System.exit(failureCount);
   }
 
 
