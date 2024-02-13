22b89697226458aee70d498570b1522a7d000a1a
John Eberhard <jeber@us.ibm.com>
2024-02-13 12:09:59 -0600

    Fix GroupTestInfo
    Signed-off-by: John Eberhard <jeber@us.ibm.com>

--- src/test/JDGroupTestInfo.java
+++ src/test/JDGroupTestInfo.java
@@ -130,7 +130,7 @@ public class JDGroupTestInfo {
         }
         // Run wrkactjob and place the results in dbfile
         String sql = "CALL QSYS2.QCMDEXC('" + "QSH CMD(''system wrkactjob > /qsys.lib/qgpl.lib/" + outputFile + ".file/"
-            + outputFile + ".mbr'')   '";
+            + outputFile + ".mbr'')   ')";
         // System.out.println("sql is "+sql);
         s.setQueryTimeout(120); /* limit is in seconds */
         try {
